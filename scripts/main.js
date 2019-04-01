aa4367c53a5a7584cbc5689893c93b8f1bf62925
Gideon <gideon30@live.nl>
2019-04-01 23:55:05 +0200

    New content 1-4-2019
    2e Api + styling

--- scripts/main.js
+++ scripts/main.js
@@ -40,6 +40,8 @@ function onAPISucces(response) {
 	// render weather in DOM
 	var weatherBox = document.getElementById('weather');
 	weatherBox.innerHTML = degC + "&#176;C <br>" + type;
+
+
 }
 
 
@@ -54,3 +56,41 @@ function onAPIError(error) {
 document.getElementById("getWeather").onclick = function(){
 	getAPIdata();
 };
+
+
+//---------------------------------------------------------------------------------------------
+
+
+// In the following example, markers appear when the user clicks on the map.
+	      // Each marker is labeled with a single alphabetical character.
+	      var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	      var labelIndex = 0;
+
+	      function initialize() {
+	        var currentlocation = { lat: 52.070499, lng: 4.300700 };
+	        var map = new google.maps.Map(document.getElementById('map'), {
+	          zoom: 12,
+	          center: currentlocation
+	        });
+
+	        // This event listener calls addMarker() when the map is clicked.
+	        google.maps.event.addListener(map, 'click', function(event) {
+	          addMarker(event.latLng, map);
+	        });
+
+	        // Add a marker at the center of the map.
+	        addMarker(currentlocation, map);
+	      }
+
+	      // Adds a marker to the map.
+	      function addMarker(location, map) {
+	        // Add the marker at the clicked location, and add the next-available label
+	        // from the array of alphabetical characters.
+	        var marker = new google.maps.Marker({
+	          position: location,
+	          label: labels[labelIndex++ % labels.length],
+	          map: map
+	        });
+	      }
+
+	      google.maps.event.addDomListener(window, 'load', initialize);
